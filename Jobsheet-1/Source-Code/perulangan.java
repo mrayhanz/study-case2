30d5d96f9a0f2bd5b33a5c3028cffca600dcf438
mrayhanz <rayymekdi@gmail.com>
2025-02-14 14:48:53 +0700

    add-img-sc

--- Jobsheet-1/Source-Code/perulangan.java
+++ Jobsheet-1/Source-Code/perulangan.java
@@ -6,7 +6,25 @@ public class perulangan {
 
         System.out.print("Masukkan NIM : ");
         String nim = sc.nextLine();
+        System.out.println("==========================");
         
-        
+        int n = Integer.parseInt(nim.substring(nim.length() - 2));  
+
+        if (n < 10) {
+            n += 10;
+        }
+
+        System.out.println("n: " + n);
+
+        for (int i = 1; i <= n; i++){
+            if (i == 6 || i == 10) {
+                continue;
+            }
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            } else {
+                System.out.print("* ");
+            }
+        }
     }
 }
