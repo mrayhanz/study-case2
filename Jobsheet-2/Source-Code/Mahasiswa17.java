3e0c50b0792ce0eca02d2deb652c0caf453ec3a6
mrayhanz <rayymekdi@gmail.com>
2025-02-21 15:40:17 +0700

    percobaan 3

--- Jobsheet-2/Source-Code/Mahasiswa17.java
+++ Jobsheet-2/Source-Code/Mahasiswa17.java
@@ -4,6 +4,17 @@ public class Mahasiswa17 {
     String kelas;
     double ipk;
 
+    Mahasiswa17(){
+        
+    }
+
+    Mahasiswa17(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
     void tampilkanInformasi(){
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
--- Jobsheet-2/Source-Code/MahasiswaMain17.java
+++ Jobsheet-2/Source-Code/MahasiswaMain17.java
@@ -1,6 +1,6 @@
 public class MahasiswaMain17 {
     public static void main(String[] args) {
-        Mahasiswa17 mhs1 = new Mahasiswa17();
+            Mahasiswa17 mhs1 = new Mahasiswa17();
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720171";
         mhs1.kelas = "SI 2J" ;
@@ -10,5 +10,9 @@ public class MahasiswaMain17 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIPK(3.60);
         mhs1.tampilkanInformasi();
-    }
+
+        Mahasiswa17 mhs2 = new Mahasiswa17("Annisa Nabila" ,"2141720160",3.25,"TI 2L");
+        mhs2.updateIPK(3.30);
+        mhs2.tampilkanInformasi();
+        }
 }
